3e435cedd72077aed6b5db2f0179797891a9d097
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2023-08-30 13:38:26 +0200

    added toast to moviesPage

--- src/pages/overviewPages/MoviesPage.tsx
+++ src/pages/overviewPages/MoviesPage.tsx
@@ -1,14 +1,30 @@
 import FilterButtons from '../../components/FilterButtons';
-
-import { useState } from 'react';
+import { toast } from 'react-toastify';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 const SeeMoviesPage = () => {
 	const [getUrl, setGetUrl] = useState<string>('')
+	const customId = 1
+
+	useEffect(() => {
+		toast(
+			<>
+				<p>Would you rather find movies by genre?</p>
+				<Link to={'/genres'}>
+					<Button variant='secondary'>To genres &raquo;</Button>
+				</Link>
+			</>
+			, { toastId: customId })
+	}, [])
 
 	return (
 		<div id="MoviesPage" className="">
 
 			<h1 className='text-center text-md-start'>Movies</h1>
+
+
 
 			<FilterButtons
 				choice={(url: string) => setGetUrl(url)}
